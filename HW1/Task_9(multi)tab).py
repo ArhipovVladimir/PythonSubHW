b8e629d1a2b080d6e483b8e3a5b8ca37157ff893
ArhipovVS <arhipof05@yndex.ru>
2023-06-05 13:17:14 +0700

    tab mult2

--- HW1/Task_9(multi)tab).py
+++ HW1/Task_9(multi)tab).py
@@ -1,5 +1,6 @@
 # Выведите в консоль таблицу умножения от 2х2 до 9х10 как на школьной тетрадке
-for i in range(n, 10):
-    for j in range(n, 10):
+for i in range(2, 10):
+    for j in range(2, 10):
         print(f'{i} x {j} = {i*j}')
     print()
+
