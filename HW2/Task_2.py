2c1488fb6b0ffd159bfe8077152b9e7e2b4d57be
ArhipovVS <arhipof05@yndex.ru>
2023-06-08 11:43:49 +0700

    HW_2 Task_2 hex2

--- HW2/Task_2.py
+++ HW2/Task_2.py
@@ -7,11 +7,12 @@
 def conv(number: int, system: int) -> list:
     dict_hex = {1: '1', 2: '2', 3: '3', 4: '4', 5: '5',
                 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A',
-                11: 'B', 12: 'C', 13: 'D', 14: 'E', 16: 'F'}
+                11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
     # dict_hex = dict(1: 1, 2: 2, 3 = 3, 4 = 4, 5 = 5, 6 = 6, 7 = 7, 8 = 8, 9 = 9, 10 = 'A', 11 = 'B', 12 = 'C')
     temp = []
     while number > 0:
-        temp.append(dict_hex(number % system))
+        elem = dict_hex[number % system]
+        temp.append(elem)
         number //= system
     temp.reverse()
     return ''.join(temp)
@@ -20,4 +21,4 @@ def conv(number: int, system: int) -> list:
 
 num, syst = map(int, input('введите число: ').split())
 print(f'Число {num} в системе {syst} исчисления рано {conv(num, syst)}')
-print(f'проверка двоичная {bin(num)}; восьмеричная {oct(num)} щеснадцатеричная {}')
+print(f'проверка двоичная {bin(num)}; восьмеричная {oct(num)} шеснадцатеричная {hex(num)}')
