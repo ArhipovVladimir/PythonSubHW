57c1eec1249b1812ba9b611b39d7368520685b43
ArhipovVS <arhipof05@yndex.ru>
2023-06-10 00:39:42 +0700

    Seminar 3 Task 5

--- Seminar_3/Task_5.py
+++ Seminar_3/Task_5.py
@@ -3,15 +3,15 @@
 нечётных элементов исходного списка.
 ✔ Нумерация начинается с единицы."""
 
-my_list = [12, 12, 15, 15, 12, 13, 16, 12, 18, 3]
+my_list = [12, 12, 15, 15, 12, 13, 16, 13, 3, 3]
 print(my_list)
 
 rez_list = []
 
-def elem_find (poz=0)
-    for item in my_list:
-        if item % 2 == 0:
-            rez_list.append(my_list.index(item)+1)
+
+for item in my_list:
+  if item % 2 != 0 and my_list.index(item) < :
+         rez_list.append(my_list.index(item)+1)
 
 
 
