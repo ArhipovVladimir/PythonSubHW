f42a37225e020eb542a4d839236575b3b54794d7
ArhipovVS <arhipof05@yndex.ru>
2023-06-10 00:06:15 +0700

    Seminar 3 Task 4

--- Seminar_3/Task_4.py
+++ Seminar_3/Task_4.py
@@ -1,2 +1,14 @@
 """✔ Создайте вручную список с повторяющимися элементами.
 ✔ Удалите из него все элементы, которые встречаются дважды."""
+
+my_list = [12, 12, 15, 15, 12, 'дом', 'окно','дом','окно','окно']
+print(my_list)
+for item in my_list:
+    if my_list.count(item) == 2:
+        dell_elem = item
+        my_list.remove(item)
+        my_list.remove(dell_elem)
+
+
+print(my_list)
+
