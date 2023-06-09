11fa6adef95f0a7292095c4b91a12a6ebdcda5c1
ArhipovVS <arhipof05@yndex.ru>
2023-06-09 23:12:01 +0700

    Seminar 3 Task 3

--- Seminar_3/Task_3.py
+++ Seminar_3/Task_3.py
@@ -5,10 +5,20 @@
 
 
 
-my_d = (1, "дом", 2, False, True, 2.4, "окно", 3.14,)
-my_type_set = set(my_d)
-print(my_type_set)
+my_tup = (1, "дом", 2, False, True, 2.4, "окно", 3.14,)
 
+dickt_res = {}
+for item in my_tup:
+    type_item = type(item)
+    if type_item in dickt_res:
+        dickt_res[type_item].append(item)
+    else:
+        dickt_res[type_item] = [item]
 
+print(dickt_res)
 
-
+dickt_res1 = {}
+for item in my_tup:
+    # dickt_res1.setdefault(type(item), []).append(item)
+    dickt_res1[type(item)] = dickt_res1.get(type(item), []) + [item]
+print(dickt_res1)
