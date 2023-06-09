4fd02df84835d7160c7460fd934fecb8e6c4aaff
ArhipovVS <arhipof05@yndex.ru>
2023-06-10 00:21:36 +0700

    Seminar 3 Task 5

--- Seminar_3/Task_5.py
+++ Seminar_3/Task_5.py
@@ -6,4 +6,13 @@
 my_list = [12, 12, 15, 15, 12, 13, 16, 12, 18, 3]
 print(my_list)
 
-for item in my_list:
+rez_list = []
+
+def elem_find (poz=0)
+    for item in my_list:
+        if item % 2 == 0:
+            rez_list.append(my_list.index(item)+1)
+
+
+
+print(rez_list)
