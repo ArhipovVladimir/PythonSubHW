77f6e0ed097d1e6b3970c300c32ef5b7196ae484
ArhipovVS <arhipof05@yndex.ru>
2023-06-10 20:38:34 +0700

    Seminar 3 Task 7

--- Seminar_3/Task_7.py
+++ Seminar_3/Task_7.py
@@ -10,3 +10,18 @@
 Объясните почему они совпадают
 или не совпадают в ваших решениях.
 """
+text = 'Вова едент в санаторий, на 12 дней'
+# text = input('Введите текст: ')
+text = text.replace(' ', '').replace(',', '').lower()
+char_list = [char for char in text]
+print(char_list)
+res_dict = {}
+for car in char_list:
+    # res_dict.setdefault(car, {}).add(char_list.count(car))
+    if car in res_dict:
+    if type(item) in dickt_res:
+        dickt_res[type(item)].append(item)
+    else:
+         dickt_res[type(item)] = [item]
+
+print(res_dict)
