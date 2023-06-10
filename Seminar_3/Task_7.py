8342b06ab6726bfaee906293ad6f44428db72c05
ArhipovVS <arhipof05@yndex.ru>
2023-06-10 21:56:24 +0700

    Seminar 3 Task 7

--- Seminar_3/Task_7.py
+++ Seminar_3/Task_7.py
@@ -11,16 +11,29 @@
 или не совпадают в ваших решениях.
 """
 #  c count
+# text = input('Введите текст: ')
+# text = text.replace(' ', '').replace(',', '').lower()
+# char_list = [char for char in text]
+# res_dict = {}
+# for car in char_list:
+#     # res_dict.setdefault(car, {}).add(char_list.count(car))
+#     if car not in res_dict:
+#           res_dict[car] = text.count(car)
+#
+# print(res_dict)
+
+# без count
+
 text = input('Введите текст: ')
 text = text.replace(' ', '').replace(',', '').lower()
 char_list = [char for char in text]
 res_dict = {}
-for car in char_list:
-    # res_dict.setdefault(car, {}).add(char_list.count(car))
-    if car not in res_dict:
-          res_dict[car] = text.count(car)
+char_list.sort()
+count = 0
+for i in len(char_list) - 1:
+    if char_list[i] == char_list [i+1]:
+        count += 1
+    else:
+        res_dict[char_list[i]] = count
 
 print(res_dict)
-
-# без count
-
