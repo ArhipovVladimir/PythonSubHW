cc4772c4fd3088630051f8f46784496768b52dcf
ArhipovVS <arhipof05@yndex.ru>
2023-06-10 19:57:16 +0700

    Seminar 3 Task 6

--- Seminar_3/Task_6.py
+++ Seminar_3/Task_6.py
@@ -3,3 +3,12 @@
 ✔ Слова выводятся отсортированными согласно кодировки Unicode.
 ✔ Текст выравнивается по правому краю так, чтобы у самого длинного
 слова был один пробел между ним и номером строки."""
+
+text = input('Введите текст: ')
+text_sort = text.split()
+text_sort.sort()
+print(text)
+print(f'{text_sort =}')
+point = len(max(text_sort))
+for number, word in enumerate(text_sort, start=1):
+    print(f'{number} {word:>{point}}')
