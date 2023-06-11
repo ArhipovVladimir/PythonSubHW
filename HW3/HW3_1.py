f7329ae8f1a5f5aac0ca58ccbc4cd4ff753c4aed
ArhipovVS <arhipof05@yndex.ru>
2023-06-11 10:19:29 +0700

    HW3_1.py

--- HW3/HW3_1.py
+++ HW3/HW3_1.py
@@ -1,2 +1,15 @@
 '''Дан список повторяющихся элементов. Вернуть список с дублирующимися
 элементами. В результирующем списке не должно быть дубликатов.'''
+
+my_list = [12, 11, 15, 15, 12, 'дом', 'окно','дом','рама','окно']
+
+#  второй вариант
+
+print(my_list)
+new_list = []
+for item in my_list:
+    if my_list.count(item) > 1 and item not in new_list:
+        new_list.append(item)
+
+print(new_list)
+
