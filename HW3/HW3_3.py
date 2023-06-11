2f53f81d4c25d1c63c83a8c82e2ce69c4f4d4c29
ArhipovVS <arhipof05@yndex.ru>
2023-06-11 14:52:37 +0700

    HW3_1.py

--- HW3/HW3_3.py
+++ HW3/HW3_3.py
@@ -3,3 +3,15 @@
 передав его максимальную грузоподъёмность. Достаточно вернуть один
 допустимый вариант. *Верните все возможные варианты комплектации рюкзака.'''
 
+stuff = {'матрас': 1, 'кострюля': 2, 'палатка': 3, 'обувь': 5, 'одеяло': 3}
+
+def backpack_capacity(capacity, stuff):
+    packaging = []
+    for item, value in stuff.items():
+        if value <= capacity:
+            capacity -= value
+            packaging.append(item)
+    return packaging
+
+
+print(f' возможный комплект: {backpack_capacity(10, stuff)}')
