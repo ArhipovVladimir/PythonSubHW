974a19bb2cf9bb188553514db05deb80dbf779a8
ArhipovVS <arhipof05@yndex.ru>
2023-06-12 15:14:09 +0700

    Semunar 3 Task1_1

--- Seminar_4/Task_1.py
+++ Seminar_4/Task_1.py
@@ -8,12 +8,15 @@
 '''
 
 def sort_word (words):
-    for number, word in enumerate(sorted(words.split()), start=1):
-        print(f'{number} {word}')
+    words_sort = sorted(words.split())
+    point =
+    print(point)
+    print(words_sort)
+    for number, word in enumerate(words_sort, start=1):
+        print(f'{number} {word:>{point}}')
 
-sort_word('Напишите функцию, которая принимает строку текста.'
-          'Вывести функцией каждое слово с новой строки.'
-          '✔ Строки нумеруются начиная с единицы.'
-          '✔ Слова выводятся отсортированными согласно кодировки Unicode.'
-          '✔ Текст выравнивается по правому краю так, чтобы у самого'
-          'длинного слова был один пробел между ним и номером строки.')
+sort_word('Напишите функцию, которая принимает строку текста. Вывести функцией каждое слово с новой строки. Строки нумеруются начиная с единицы. Слова выводятся отсортированными согласно кодировки длинного слова был один пробел между ним и номером строки.')
+
+# point = len(max(text_sort))
+# for number, word in enumerate(text_sort, start=1):
+#     print(f'{number} {word:}')
