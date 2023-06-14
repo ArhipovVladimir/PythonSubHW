85cbc5199282472af16a3a3ac7a93e7fd22ac41b
ArhipovVS <arhipof05@ayndex.ru>
2023-06-14 18:00:49 +0700

    Refaktor bankomat

--- Seminar_4/HW_4.py
+++ Seminar_4/HW_4.py
@@ -9,3 +9,15 @@
 на отдельные операции — функции. Дополнительно сохраняйте
 все операции поступления и снятия средств в список.
 '''
+
+def hashable_dicts(**kwargs):
+    pets = dict()
+    for k, v in kwargs.items():
+        if isinstance(v, (list, dict, set, bytearray)):
+            v = str(v)
+        pets[v] = k
+    return pets
+
+
+print(hashable_dicts(dog='Jack', cat={'Leopold': 2, 'Murka': 3}, turtle=['bill', 'jack', 'anatoliy'],
+                     hamster={'edward', 'homa'}))
