4521829ad0d529811d05613acd1bbf3651402f6e
ArhipovVS <arhipof05@ayndex.ru>
2023-06-17 19:50:03 +0700

    analis

--- HW4/Task_7.py
+++ HW4/Task_7.py
@@ -6,3 +6,20 @@
 прибыльные, верните истину, а если хотя бы одна убыточная — ложь.
 '''
 
+def copmani_analiti (data_comp):
+    list_res = []
+    for item in data_comp.values():
+        if (sum(item)) > 0:
+            list_res.append(True)
+        else: list_res.append(False)
+    return all(list_res)
+
+
+    # return all(map(lambda x: sum(x) > 0, data_comp))
+
+
+data_compani = {"OOO": [100, -50, -20],
+                "AO": [120, -80, -20],
+                "GOV": [200, -20, -30]}
+
+print(copmani_analiti(data_compani))
