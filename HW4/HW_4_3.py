fe7aefba1044ade79b129112b85d80e219439eea
ArhipovVS <arhipof05@ayndex.ru>
2023-06-17 12:41:16 +0700

    Refaktor bankomat

--- HW4/HW_4_3.py
+++ HW4/HW_4_3.py
@@ -1,6 +1,7 @@
 ''''✔ Возьмите задачу о банкомате из семинара 2. Разбейте её
 на отдельные операции — функции. Дополнительно сохраняйте
 все операции поступления и снятия средств в список.'''
+import datetime
 
 START_SUM = 0
 DEV = 50
@@ -44,6 +45,10 @@ def valid_oper (dev_sum):
         if sum_oper % dev_sum == 0:
             return sum_oper
 
+def history_file(*arg):
+    with open('histoty.txt', 'a') as h:
+        time_date = str(datetime.datetime.now())
+        h.write(f'{time_date}  {arg}\n')
 
 account = START_SUM
 count = 1
@@ -58,6 +63,9 @@ while True:
             account = add_money(account, cahe_oper, flag)
             print(f'останток на счете  {account}')
             count += 1
+            history_file(cahe_oper, 'add')
+
+
     elif choice == 2:
             cahe_oper = valid_oper(DEV)
             if count % COUNT_BONUS_OPER == 0:
@@ -69,6 +77,7 @@ while True:
             else:
                 print(f'останток на счете  {account} процент за снятие {morj}')
                 count += 1
+            history_file(cahe_oper, 'pop morj - ', morj)
     elif choice == 3:
             if account > TASK_RICH:
                 print(f'налог  {round(account * TASK, 2)}')
