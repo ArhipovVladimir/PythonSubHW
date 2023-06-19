508e13f85b07cf5a08e4315e1cd04d0142ce59b2
ArhipovVS <arhipof05@ayndex.ru>
2023-06-19 14:45:46 +0700

    HW 5 Task 3 fibo ok

--- HW5/HW_5_Task_3.py
+++ HW5/HW_5_Task_3.py
@@ -4,6 +4,23 @@
 
 
 def gen_fibo(num):
-
-    if num == 1:
+    if num == 0:
+        yield 0
+    elif num == 1:
         yield 1
+    f1 = 0
+    f2 = 1
+    for _ in range(num):
+        f3 = f1 + f2
+        f1 = f2
+        f2 = f3
+        yield f3
+
+
+
+fibo_user = gen_fibo(20)
+print(*fibo_user)
+
+
+
+
