8f6d830b13e2ace549facc60cef998d7069c269b
ArhipovVS <arhipof05@ayndex.ru>
2023-06-20 17:15:23 +0700

    Seminar 6 Task 3

--- Seminar_6/Task_2.py
+++ Seminar_6/Task_2.py
@@ -6,3 +6,32 @@
 Если число угадано, возвращается истина, а если попытки исчерпаны - ложь.
 
 '''
+import random
+
+
+def funk_round(min_lim, max_lim, level):
+    digit = random.randint(min_lim, max_lim)
+    while level > 0:
+        print(f'Веедите число между {min_lim} и {max_lim} у Вас {level} попыток')
+        num = int(input())
+        if num < min_lim or num > max_lim:
+            print(f'Число {num} не в границах интервала {min_lim} {max_lim}')
+            level -= 1
+        elif num < digit:
+            print(f'Число {num} меньше загаданного')
+            level -= 1
+        elif num > digit:
+            print(f'Число {num} больше загаданного')
+            level -= 1
+        else:
+            print(f'правильно компьютер загадывал {num}')
+            return True
+
+    else:
+        print(f' колическо попыток ичерпано - компьютер загадывал {digit}')
+        return False
+
+
+min_lim, max_lim, level = map(int, input('Enter min_lim, max_lim, level: ').split())
+funk_round(min_lim, max_lim, level)
+
--- Seminar_6/Task_3.py
+++ Seminar_6/Task_3.py
@@ -2,3 +2,33 @@
 Добавьте возможность запуска функции “угадайки” из модуля в командной строке терминала.
 Строка должна принимать от 1 до 3 аргументов: параметры вызова функции.
 Для преобразования строковых аргументов командной строки в числовые параметры используйте генераторное выражение.'''
+
+import random
+from sys import argv
+
+def funk_round(min_lim, max_lim, level):
+    digit = random.randint(min_lim, max_lim)
+    while level > 0:
+        print(f'Веедите число между {min_lim} и {max_lim} у Вас {level} попыток')
+        num = int(input())
+        if num < min_lim or num > max_lim:
+            print(f'Число {num} не в границах интервала {min_lim} {max_lim}')
+            level -= 1
+        elif num < digit:
+            print(f'Число {num} меньше загаданного')
+            level -= 1
+        elif num > digit:
+            print(f'Число {num} больше загаданного')
+            level -= 1
+        else:
+            print(f'правильно компьютер загадывал {num}')
+            return True
+
+    else:
+        print(f' колическо попыток ичерпано - компьютер загадывал {digit}')
+        return False
+
+
+min_lim, max_lim, level = map(int, argv.split())
+funk_round(min_lim, max_lim, level)
+
