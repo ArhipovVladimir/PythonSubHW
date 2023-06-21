d02c49804a39fb339121ea94a919db5a9adac0d9
ArhipovVS <arhipof05@ayndex.ru>
2023-06-21 14:23:41 +0700

    INIT HW 6

---
+++ HW6/HW6_Task_1.py
@@ -0,0 +1,47 @@
+
+
+def check_day(dd, mm, yyyy):
+    list_monat = [1, 3, 5, 7, 8, 10, 12]
+    if dd > 31:
+        return False
+    elif dd == 31 and mm not in list_monat:
+        return False
+    elif dd == 29 and mm == 2:
+        if _check_yare(yyyy):
+            return True
+        return False
+    return True
+
+
+def check_monat(mm):
+    if mm > 12:
+        return False
+    return True
+
+
+def _check_yare(yyyy):
+    if yyyy % 4 != 0:
+        return False
+    elif yyyy % 100 == 0:
+        if yyyy % 400 == 0:
+            return True
+        return False
+    else:
+        return True
+
+
+def check_date(dd, mm, yyyy):
+    return all([check_day(dd, mm, yyyy), check_monat(mm)])
+
+
+if __name__ == '--main__':
+
+    while True:
+        dd, mm, yyyy = map(int, input('введите дату в формае DD.MM.YYYY в диапазоне [1, 9999]: ').split('.'))
+        if 1 < yyyy <= 9999:
+            break
+
+    if check_date(dd, mm, yyyy):
+        print('дата может существовать')
+    print('дата не возможна')
+
