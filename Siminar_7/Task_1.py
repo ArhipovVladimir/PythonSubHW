c2ec1857b441ee7103c7b0f82335d2248d77073d
ArhipovVS <arhipof05@yndex.ru>
2023-06-26 21:55:43 +0700

    Seminar 7 Task 1 - 2

--- Siminar_7/Task_1.py
+++ Siminar_7/Task_1.py
@@ -6,3 +6,19 @@
 ✔ Минимальное число - -1000, максимальное - +1000.
 ✔ Количество строк и имя файла передаются как аргументы функции.
 '''
+
+
+from random import randint, uniform
+MIN_NUM = -1000
+MAX_NUM = 1000
+
+
+def get_file(count_str, name_file):
+    with open(f'{name_file}', 'w') as f:
+        for _ in range(count_str):
+            f.write(f'{randint(MIN_NUM, MAX_NUM)} | '
+                         f'{uniform(MIN_NUM ,MAX_NUM)}\n')
+
+
+
+get_file(20, 'file_num.txt')
--- Siminar_7/Task_2.py
+++ Siminar_7/Task_2.py
@@ -6,5 +6,26 @@
 состоять из 4-7 букв, среди которых
 обязательно должны быть гласные.
 ✔ Полученные имена сохраните в файл.
-
 """
+from random import randint
+MIN_LEN = 4
+MAX_LEN = 7
+
+def get_name_file():
+    len_name_file = randint(MIN_LEN, MAX_LEN + 1)
+    str_name = ''
+    # map(lambda x: x = chr(randint(97, 122)) for _ in range(len_name_file))
+    for _ in range(len_name_file):
+        str_name += chr(randint(97, 122))
+
+    str_name=str_name.capitalize()
+
+    with open('file_name.txt', 'a') as f:
+        f.write(f'{str_name}\n')
+
+
+
+
+
+
+get_name_file()
