4255c6aeb6f0dd1798525e0f3c4468a96c81f94a
ArhipovVS <arhipof05@yndex.ru>
2023-06-29 13:42:05 +0700

    Seminar 8 Task 1 - 3

--- Seminar_8/Task_3.py
+++ Seminar_8/Task_3.py
@@ -11,18 +11,17 @@ import json
 def create_csv(file_json, file_csv):
 
     with (open(file_json, 'r', encoding='utf-8') as f_read,
-          open(file_csv, 'w', encoding='utf-8')as f_write):
+          open(file_csv, 'w', encoding='utf-8', newline='')as f_write):
 
         dict_json = json.load(f_read)
-        csv_write = csv.DictWriter(f_write, 'excel_tab', fieldnames=["level", "user_id", "user_name"],
-                                   quoting=csv.QUOTE_ALL)
+        csv_write = csv.DictWriter(f_write, fieldnames=["level", "user_id", "user_name"])
         csv_write.writeheader()
         all_data = []
-        for i, level in enumerate(dict_json):
-            for j, user_id, in enumerate(dict_json[level]):
-                if i != 0 and j != 0:
-                    all_data.append(level, user_id, dict_json[level][user_id])
+        for level in dict_json:
+            for user_id, in dict_json[level]:
+                    all_data.append({'level': level, 'user_id': user_id, 'user_name': dict_json[level][user_id]})
 
+        print(all_data)
         csv_write.writerows(all_data)
 
     # print(dict_user)
