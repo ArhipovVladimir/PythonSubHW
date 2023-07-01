931ae24f1755d3115e63eecb11a527a7b19aa697
ArhipovVS <arhipof05@yndex.ru>
2023-07-01 11:18:44 +0700

    Seminar 8 HW 8

--- HW8/HW8_Task8.py
+++ HW8/HW8_Task8.py
@@ -17,10 +17,10 @@ import csv
 
 def walk_write(dir_ = os.getcwd()):
     res_list = []
-
+    dict_dir_file = {}
     for patch_, dir_names, file_names in os.walk(dir_):
-        # print(patch_, dir_names, file_names )
-        dict_dir_file = {}
+        print(patch_, dir_names, file_names )
+
         # if len(dir_names) == 0 or len(file_names) == 0:
         dict_dir_file.setdefault("patch", {patch_}).add(patch_)
         for dir_name in dir_names:
@@ -33,40 +33,40 @@ def walk_write(dir_ = os.getcwd()):
         for file_name in file_names:
             # if file_name in dict_dir_file.values():
                 dict_dir_file.setdefault("File", {file_name}).add(file_name)
-                dict_dir_file.setdefault("size", {os.path.getsize(os.path.join(patch_, file_name))})\
+                dict_dir_file.setdefault(f'{file_name}_size', {os.path.getsize(os.path.join(patch_, file_name))})\
                     .add(os.path.getsize(os.path.join(patch_, file_name)))
                 res_size = res_size + os.path.getsize(os.path.join(patch_, file_name))
         dict_dir_file.setdefault('Tolal_size', res_size)
-    res_list.append(dict_dir_file)
+        res_list.append(dict_dir_file)
     print(res_list)
 
     # for patch_, dir_names, file_names in os.walk(dir_):
     #     print(patch_, dir_names, file_names )
         # print(len(patch_), len(dir_names), len(file_names))
 
-    # with(
-    #     open('dict_dir_file.json', 'w', encoding='utf-8') as f_json,
-    #     open('dict_dir_file.pickle', 'wb') as f_pic,
-    #     open('dict_dir_file.csv', 'w', encoding='utf-8', newline='') as f_csv
-    # ):
-        # csv_write = csv.DictWriter(f_csv, dialect='excel', fieldnames=['patch', 'dir', 'file_name', 'size', 'Total_size'])
-        # csv_write.writeheader()
-        # all_data = []
-        # for info in os.walk(dir_):
-        #     str_data = {}
-        #     for p, key, value in info:
-        #         str_data.setdefault(key, value)
-        #     all_data.append(str_data)
-        # print(all_data)
-        #
-        # csv_write.writerows(all_data)
+    with(
+         open('dict_dir_file.json', 'w', encoding='utf-8') as f_json,
+         open('dict_dir_file.pickle', 'wb') as f_pic,
+         open('dict_dir_file.csv', 'w', encoding='utf-8', newline='') as f_csv
+     ):
+        csv_write = csv.DictWriter(f_csv, dialect='excel', fieldnames=['patch', 'dir', 'file_name', 'size', 'Total_size'])
+        csv_write.writeheader()
+        all_data = []
+        for info in os.walk(dir_):
+            str_data = {}
+            for p, key, value in info:
+                str_data.setdefault(key, value)
+            all_data.append(str_data)
+        print(all_data)
+
+        csv_write.writerows(all_data)
 
 
-        # json.dump(dict_dir_file, f_json, ensure_ascii=False)
-        # pickle.dump(dict_dir_file, f_pic)
+        json.dump(dict_dir_file, f_json, ensure_ascii=False)
+        pickle.dump(dict_dir_file, f_pic)
 
 
 
 if __name__ == '__main__':
-    # walk_write('C:\\Users\\arhip\\OneDrive\\Документы\\GB\\Python2\\PythonSubHW\\HW6')
-    walk_write()
+    walk_write('C:\\Users\\arhip\\OneDrive\\Документы\\GB\\Python2\\PythonSubHW\\HW6')
+    # walk_write()
