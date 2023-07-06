72f6355f777ec6db5c5a1d9cb2d5eda5a30245cb
ArhipovVS <arhipof05@yndex.ru>
2023-07-06 23:25:09 +0700

    Seminar 10 HW 1-2

--- Seminar_10/Task_4.py
+++ Seminar_10/Task_4.py
@@ -14,18 +14,23 @@ from random import randint
 class Person(Human):
 
     GET = 7
-    def __init__(self, id_num, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
+        
+        def get_id_num(self):
+            return randint(100000, 999999)
+
         def get_access(self):
             return sum(map(int, str(self.id_num))) % 7
 
+
         super().__init__(*args, **kwargs)
-        self.id_num = randint(100000, 999999)
+        self.id_num = get_id_num
         self.access = get_access(self)
 
 
 if __name__ == '__main__':
 
-    p2 = Person(15, 'Ivan', 'Petrov', 'Sidorovich', 25)
+    p2 = Person('Ivan', 'Petrov', 'Sidorovich', 25)
     print(p2.full_name())
     print(p2.get_age())
     p2.birthday()
