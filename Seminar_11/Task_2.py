4d652668a1254d30dc5a53d1a04cdbbdc6d1731c
ArhipovVS <arhipof05@yndex.ru>
2023-07-09 22:54:17 +0700

    Seminar 11 Task 1

--- Seminar_11/Task_2.py
+++ Seminar_11/Task_2.py
@@ -7,25 +7,29 @@
 list-архивы также являются свойствами экземпляра
 """
 
-class Archive(int):
-    list_arhive = []
+class Archive:
+    list_num = []
+    list_str =[]
 
-    def __new__(cls, value):
-        intanse = super().__new__(cls, value)
-        intanse.list_arhive.append(value)
-        return intanse
+     # def __new__(cls, *args, **kwargs):
+     #      intanse = super().__new__(cls, *args, **kwargs)
+     #      intanse.list_arhive.append(*args, **kwargs)
+     #      return intanse
 
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, number, strg):
+        self.number = number
+        self.strg = strg
+        self.list_num.append(number)
+        self.list_str.append(strg)
 
 
 if __name__ == '__main__':
-    i = Archive(5)
-    print(i)
-    print(i.list_arhive)
-    i2 = Archive(6)
+    i = Archive(5, 'grhdsq')
+    print(f'{i}')
+    print(i.list_str, i.list_num)
+    i2 = Archive(6, '120000')
     print(i2)
-    print(i2.list_arhive)
-    i3 = Archive(7)
-    print(i3)
-    print(i3.list_arhive)
+    print(i2.list_num, i2.list_str)
+    # i3 = Archive(7)
+    # print(i3)
+    # print(i3.list_arhive)
