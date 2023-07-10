30c42d28abb5f0204dfd9153a92993eb701e57ad
ArhipovVS <arhipof05@yndex.ru>
2023-07-10 22:35:26 +0700

    Seminar 11 Task 3-4

--- Seminar_11/Task_3.py
+++ Seminar_11/Task_3.py
@@ -1,2 +1,33 @@
 """Задание №3
 Добавьте к задачам 1 и 2 строки документации для классов."""
+
+class Archive:
+    """"Мой трен6ировоный класс"""
+
+    list_num = []
+    list_str =[]
+
+     # def __new__(cls, *args, **kwargs):
+     #      intanse = super().__new__(cls, *args, **kwargs)
+     #      intanse.list_arhive.append(*args, **kwargs)
+     #      return intanse
+
+    def __init__(self, number, strg):
+        """добавление экземпляра в класс"""
+        self.number = number
+        self.strg = strg
+        self.list_num.append(number)
+        self.list_str.append(strg)
+
+
+if __name__ == '__main__':
+    i = Archive(5, 'grhdsq')
+    print(f'{i}')
+    print(i.list_str, i.list_num)
+    i2 = Archive(6, '120000')
+    print(i2)
+    print(i2.list_num, i2.list_str)
+    help(Archive)
+    # i3 = Archive(7)
+    # print(i3)
+    # print(i3.list_arhive)
--- Seminar_11/Task_4.py
+++ Seminar_11/Task_4.py
@@ -4,3 +4,42 @@
 Добавьте методы представления экземпляра для программиста
 и для пользователя.
 """
+
+class Archive:
+    """"Мой трен6ировоный класс"""
+
+    list_num = []
+    list_str =[]
+
+     # def __new__(cls, *args, **kwargs):
+     #      intanse = super().__new__(cls, *args, **kwargs)
+     #      intanse.list_arhive.append(*args, **kwargs)
+     #      return intanse
+
+    def __init__(self, number, strg):
+        """добавление экземпляра в класс"""
+        self.number = number
+        self.strg = strg
+        self.list_num.append(number)
+        self.list_str.append(strg)
+
+    def __str__(self):
+        return f' текущий экземпляр {self.number} Архивы {self.list_str} и {self.list_num}'
+
+    def __repr__(self):
+        return f'Arhive({self.number}, {self.strg})'
+
+
+if __name__ == '__main__':
+    i = Archive(5, 'grhdsq')
+    print(i)
+    print(f'{i = }')
+    i2 = Archive(6, '120000')
+    print(i2)
+    print(f'{i2 = }')
+
+
+
+    # i3 = Archive(7)
+    # print(i3)
+    # print(i3.list_arhive)
