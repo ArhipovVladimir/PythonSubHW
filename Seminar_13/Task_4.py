b7e88baffd3210c61130a52f16150c76ebf3a75f
ArhipovVS <arhipof05@yndex.ru>
2023-07-16 22:52:58 +0700

    seminar 13 Task 1

--- Seminar_13/Task_4.py
+++ Seminar_13/Task_4.py
@@ -6,39 +6,28 @@
 информацию в JSON файл.
 Напишите класс пользователя, который хранит эти данные в
 свойствах экземпляра.
+Магический метод на равенство пользователей
+
 Отдельно напишите функцию, которая считывает информацию
 из JSON файла и формирует множество пользователей.
 
 """
+class User:
 
-import json
-import os
+    def __init__(self, user_di, user, level):
+        self.user_id = user_di
+        self.user = user
+        self.level = level
 
-
-def user_add_json(file_mame):
-    file_mame = f'{file_mame}.json'
-    print(os.listdir())
-    if file_mame in os.listdir():
-        with open(file_mame, 'r', encoding='utf-8') as f1:
-            dict_user = json.load(f1)
-    else: dict_user = {str(i): {} for i in range(1, 8)}
-
-    while True:
-        data = input('введите имя пользователя')
-        if not data:
-            break
-
-        user_name, user_id, access = data.split()
-
-        if user_id in dict_user[access]:
-            print("id дублируется")
-            continue
-
-        dict_user.setdefault(access, {user_id: user_name})[user_id] = user_name
-
-    with open(file_mame, 'r', encoding='utf-8') as f2:
-        json.dump(dict_user, f2, ensure_ascii=False)
+    def __eq__(self, other):
+        return self.user_id == other.user_id and \
+            self.user == other.user and \
+            self.level == other.level
 
 
 if __name__ == '__main__':
-    user_add_json('user_id')
+    u1 = User("1", "Владимир", "1")
+    u2 = User("4", "Никита", "1")
+    u3 = User("4", "Никита", "1")
+    print(u1 == u2)
+    print(u3 == u2)
