7718b9f0d38f6f07dea6ed6fa5fd88b7d9ebada1
ArhipovVS <arhipof05@yndex.ru>
2023-07-16 21:22:08 +0700

    seminar 13 Task 1

--- Seminar_13/Task_1.py
+++ Seminar_13/Task_1.py
@@ -6,3 +6,21 @@
 Обрабатывайте не числовые данные как исключения.
 """
 
+
+def get_num(text):
+
+    while True:
+        data = input(text)
+        try:
+            return int(data)
+        except ValueError as e:
+            try:
+                return float(data)
+            except ValueError as e:
+                print(f'не число ошибка {e}')
+
+
+if __name__ == '__main__':
+    number = get_num('введите число ')
+    print(number)
+
