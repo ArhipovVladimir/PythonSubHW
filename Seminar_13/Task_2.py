a4bcece1f3369674edf17ee981f80570c7908b8d
ArhipovVS <arhipof05@yndex.ru>
2023-07-16 22:25:53 +0700

    seminar 13 Task 1

--- Seminar_13/Task_2.py
+++ Seminar_13/Task_2.py
@@ -9,3 +9,22 @@
 
 """
 
+
+def get_dict(dict_use, key, defalt):
+    try:
+        return dict_use[key]
+    except KeyError:
+        return defalt
+
+
+if __name__ == '__main__':
+    dct = {
+        1: 'a',
+        2: 'a',
+        3: 'c',
+        4: 'd'
+    }
+
+    get_dict(dct, 5, 'e')
+    print(get_dict(dct, 4, 'f'))
+    print(dct)
