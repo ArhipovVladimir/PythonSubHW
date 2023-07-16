f5552d76bd0a3eedc7341a6001f6f57f928bf7ae
ArhipovVS <arhipof05@yndex.ru>
2023-07-16 15:18:30 +0700

    Seminar 12 init

--- Seminar_12/HW12.py
+++ Seminar_12/HW12.py
@@ -9,16 +9,21 @@
 ○ Также экземпляр должен сообщать средний балл по тестам для каждого
 предмета и по оценкам всех предметов вместе взятых."""
 
-# str.isalpha()
-# str.istitle()
+from HW12_1 import gen_csv_test
+from HW12_1 import gen_csv_order
+import csv
+from statistics import mean
+
 
 class GetFulLName:
 
+
     def __set_name__(self, owner, name):
-        self.param_name = name
+        self.param_name = name.split(' ')
 
     def __set__(self, instance, value):
         self.validate(value)
+        # setattr(instance, self.param_name, value)
         self.param_name = value
 
     def validate(sels, value):
@@ -26,22 +31,79 @@ class GetFulLName:
             raise ValueError(f'в строке должны быть только буквы {value}')
         if not value.istitle():
             raise ValueError(f' ФИО не с большой буквы {value}')
+
+
+def Readfile(first_name, last_name):
+    gen_csv_test (f'{first_name} {last_name}')
+    gen_csv_order (f'{first_name} {last_name}')
+    file_order = f'{first_name} {last_name}_order.csv'
+    file_test = f'{first_name} {last_name}_test.csv'
+
+
+    with (open(file_order, 'r', encoding='utf-8', newline='') as f_ord,
+          open(file_test, 'r', encoding='utf-8', newline='') as f_test):
+
+            csv_file = csv.reader(f_ord)
+            lst_ord = []
+            file = [*csv_file]
+            a, b, c, d, e = file[0]
+
+            for a_ord, b_ord, c_ord, d_ord, e_ord in file[1:]:
+                lst_ord.append({a: int(a_ord), b: int(b_ord), c: int(c_ord),
+                                d: int(d_ord), e: int(e_ord)})
+
+            csv_file = csv.reader(f_test)
+            lst_test = []
+            file = [*csv_file]
+            a, b, c, d, e = file[0]
+
+            for a_ord, b_ord, c_ord, d_ord, e_ord in file[1:]:
+                lst_test.append({a: int(a_ord), b: int(b_ord), c: int(c_ord), d:
+                                    int(d_ord), e: int(e_ord)})
+
+
+            return lst_ord, lst_test
+
+
+
 class Student:
 
-    name_stud = GetFulLName()
-    first_name = GetFulLName()
-    def get_predmet(self):
+    # first_name = GetFulLName()
+    # last_name = GetFulLName()
 
-         return
-    def __init__(self, name_stud, first_name):
-        self.name_stud = name_stud
+
+
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
-        self.full_name = self.name_stud + self.first_name
-        self._predm = self.get_predmet(self.full_name)
+        self.last_name = last_name
+        self.order, self.test = Readfile(self.first_name, self.last_name)
+
 
     def __str__(self):
-        return f'Имя {self.name_stud} Фамилия {self.name_stud}'
+        return f'{self.last_name} {self.first_name} {self.order}'
+
+    def get_mean(self, predmrt):
+        list_ord_predmet = []
+        for i in range(len(self.order)):
+            for k, v in self.order[i].items():
+                if k == predmrt:
+                    list_ord_predmet.append(v)
+        print(f'{self.last_name} среднее {mean(list_ord_predmet)} по {predmrt}')
+
+    def get_test(self):
+        # print(self.test[0])
+        for k, v in self.test[0].items():
+             print(f'{self.last_name} итоговые тесты {k} {v}')
+
+
+
 
 if __name__ == '__main__':
-    s1 = Student('Иван', 'Петров')
-    print(s1)
+    s2 = Student('Иван', 'Петров')
+    print(s2)
+    s3 = Student('Владимир', 'Архипов')
+    print(s3)
+    print(s2.get_mean('химия'))
+    print(s3.get_mean('физика'))
+
+    print(s3.get_test())
--- Seminar_12/HW12_1.py
+++ Seminar_12/HW12_1.py
@@ -1,28 +1,41 @@
 """Gen predmet"""
 
-from random import randint
+from random import choice, randint
 import csv
 COUNT_ORDER = 21
-def gen_csv (min_order=0, max_order=5, max_test=0, min_test=100, stud_name="file"):
-      with open(f'{stud_name}.csv', 'w', encoding='utf-8', newline='') as f:
+MIN_TEST = 0
+MAX_TEST = 100
+def gen_csv_order (stud_name="file"):
+      with open(f'{stud_name}_order.csv', 'w', encoding='utf-8', newline='') as f:
         predmets = ['химия', 'физика', 'математика', 'русский', 'информатика']
         csv_write = csv.DictWriter(f, fieldnames=predmets)
         csv_write.writeheader()
+        lisl_order = [2, 3, 4, 5]
         for _ in range(COUNT_ORDER):
             row_order = []
-            row_order.append({'химия': randint(min_order, max_order),
-                                   'физика': randint(min_order, max_order),
-                                   'математика': randint(min_order, max_order),
-                                   'русский': randint(min_order, max_order),
-                                   'информатика': randint(min_order, max_order)})
+            row_order.append({'химия': choice(lisl_order),
+                                   'физика': choice(lisl_order),
+                                   'математика': choice(lisl_order),
+                                   'русский': choice(lisl_order),
+                                   'информатика': choice(lisl_order)})
             csv_write.writerows(row_order)
-        row_order.append({'химия': randint(max_test, min_test),
-                              'физика': randint(max_test, min_test),
-                              'математика': randint(max_test, min_test),
-                              'русский': randint(max_test, min_test),
-                              'информатика': randint(max_test, min_test)})
+
+
+def gen_csv_test(stud_name="file"):
+    with open(f'{stud_name}_test.csv', 'w', encoding='utf-8', newline='') as f:
+        predmets = ['химия', 'физика', 'математика', 'русский', 'информатика']
+        csv_write = csv.DictWriter(f, fieldnames=predmets)
+        csv_write.writeheader()
+        row_order = [{'химия': randint(MIN_TEST, MAX_TEST),
+                          'физика': randint(MIN_TEST, MAX_TEST),
+                          'математика': randint(MIN_TEST, MAX_TEST),
+                          'русский': randint(MIN_TEST, MAX_TEST),
+                          'информатика': randint(MIN_TEST, MAX_TEST)}]
         csv_write.writerows(row_order)
 
 
+
 if __name__ == '__main__':
-    gen_csv(2, 5, 0, 100, 'Иван Петров')
+    gen_csv_order('Иван Петров')
+    gen_csv_test('Иван Петров')
+
