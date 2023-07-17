c19056b0ee927f40c86424e0922d1c8e0c2ee83a
ArhipovVS <arhipof05@yndex.ru>
2023-07-18 01:08:29 +0700

    seminar 13 Task 5

--- Seminar_13/Task_4.py
+++ Seminar_13/Task_4.py
@@ -14,22 +14,37 @@
 """
 class User:
 
-    def __init__(self, user_di, user, level=None):
-        self.user_id = user_di
+    def __init__(self, user, user_id, level=None):
+        self.user_id = user_id
         self.user = user
         self.level = level
 
     def __eq__(self, other):
-        return self.user_id == other.user_id and self.user == other.user
+        return self.user == other.user and self.user_id == other.user_id
 
     def __str__(self):
         return f'польз {self.user} id {self.user_id} уровень {self.level}'
+
+    # def __iter__(self):
+    #     return self
+    #
+    # def __next__(self):
+    #         return self
+
+        # raise StopIteration
+
+
+
 
 
 if __name__ == '__main__':
     u1 = User("1", "Владимир", "1")
     u2 = User("4", "Никита", "1")
     u3 = User("4", "Никита", "1")
+    u4 = User("2", "Иван", "1")
+    u5 = User("2", "Иван", "1")
     print(u1 == u2)
     print(u3 == u2)
-    print(u1, u2, u3, sep='\n')
+    print(u3 == u4)
+    print(u4 == u5)
+    print(u1, u2, u3, u4, u5, sep='\n')
--- Seminar_13/Task_5.py
+++ Seminar_13/Task_5.py
@@ -17,17 +17,50 @@
 * метод удаления пользователя из списка пользователей проекта
 * метод сохранения списка пользователей в JSON файл при выходе из контекстного менеджера
 """
+import json
+
 from Task_4 import User
+from Task_3 import AccessError, LevelError
 
 class Project:
 
-    def __init__(self):
-        self.list_user =
-        self.admim_project =
+    def __init__(self, list_user):
+        self.list_user = list_user
+        self.admim_project = None
 
-    # def (cls)
+    @classmethod
+    def load_json (cls, json_file):
+            list_user = []
+            with open(json_file, 'r', encoding='utf-8') as f_read:
+                user_dict = json.load(f_read)
 
-    # def enter:
-        user_name, id_user  = input('введите имя пользователя и id').split(" ")
-        user = User(user_name, id_user)
-    def add_user
+            for level, in user_dict:
+                for user_id in user_dict[level]:
+                    list_user.append(User(user_dict[level][user_id], user_id, level))
+
+            return Project(list_user)
+
+    def enter(self):
+        user_name, id_user = input('введите имя пользователя и id').split(" ")
+        user_new = User(user_name, id_user)
+        # print(user)
+        if user_new not in self.list_user:
+            raise AccessError(user_name)
+
+        for user in self.list_user:
+             if user == user_new:
+                self.admim_project = user
+
+    # def add_user
+
+    def __str__(self):
+        return f'пользователи {self.list_user} админ {self.admim_project}'
+
+
+
+if __name__ == '__main__':
+    p = Project.load_json('user_id.json')
+    # print(p)
+    p.enter()
+    print(p)
+
