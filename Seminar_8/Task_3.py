13c1894e03ea0fad31f887a553565f0d6b40c6de
ArhipovVS <arhipof05@yndex.ru>
2023-07-20 23:33:55 +0700

    HW 13

--- Seminar_8/Task_3.py
+++ Seminar_8/Task_3.py
@@ -11,21 +11,25 @@ import json
 
 def create_json_csv(file_json, file_csv):
 
-    with (open(file_json, 'r', encoding='utf-8') as f_read,
-          open(file_csv, 'w', encoding='utf-8', newline='')as f_write):
+    try:
+        with (open(file_json, 'r', encoding='utf-8') as f_read,
+              open(file_csv, 'w', encoding='utf-8', newline='')as f_write):
+            dict_json = json.load(f_read)
+            csv_write = csv.DictWriter(f_write, fieldnames=["level", "user_id", "user_name"])
+            csv_write.writeheader()
+            all_data = []
+            for level in dict_json:
+                for user_id, user in dict_json.items():
+                    all_data.append({'level': level, 'user_id': user_id, 'user_name': user})
 
-        dict_json = json.load(f_read)
-        csv_write = csv.DictWriter(f_write, fieldnames=["level", "user_id", "user_name"])
-        csv_write.writeheader()
-        all_data = []
-        for level in dict_json:
-            for user_id, in dict_json[level]:
-                    all_data.append({'level': level, 'user_id': user_id, 'user_name': dict_json[level][user_id]})
+            print(all_data)
+            csv_write.writerows(all_data)
+    except FileNotFoundError as e:
+           print(f' файл не найден {e}')
 
-        print(all_data)
-        csv_write.writerows(all_data)
+
 
     # print(dict_user)
 
 if __name__ == '__main__':
-    create_json_csv('user_id.json', 'user_id.csv')
+    create_json_csv('user_id_v1.json', 'user_id.csv')
