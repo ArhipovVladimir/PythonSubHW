4e4e4eef8c9d0ce500cb9d7c4ad6d8e5b6262647
ArhipovVS <arhipof05@yndex.ru>
2023-07-22 10:37:50 +0700

    HW 13

--- Seminar_8/Task_2_1.py
+++ Seminar_8/Task_2_1.py
@@ -12,3 +12,35 @@ JSON файл.
 При перезапуске функции уже записанные в файл данные
 должны сохраняться
 """
+
+import json
+import os
+
+
+def user_add_json(file_mame):
+    file_mame = f'{file_mame}.json'
+    print(os.listdir())
+    if file_mame in os.listdir():
+        with open(file_mame, 'r', encoding='utf-8') as f1:
+            dict_user = json.load(f1)
+    else: dict_user = {str(i): {} for i in range(1, 8)}
+
+    while True:
+        data = input('введите имя пользователя')
+        if not data:
+            break
+
+        user_name, user_id, access = data.split()
+
+        if user_id in dict_user[access]:
+            print("id дублируется")
+            continue
+
+        dict_user.setdefault(access, {user_id: user_name})[user_id] = user_name
+
+    with open(file_mame, 'w', encoding='utf-8') as f2:
+        json.dump(dict_user, f2, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    user_add_json('user_id')
