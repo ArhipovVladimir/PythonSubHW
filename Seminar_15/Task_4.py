fd910f482efe3c76904ad6ee0157d8570b28edc3
ArhipovVS <arhipof05@yndex.ru>
2023-07-23 17:55:07 +0700

    Seminar 15 Task 4

--- Seminar_15/Task_4.py
+++ Seminar_15/Task_4.py
@@ -7,24 +7,63 @@
 
 """
 from datetime import datetime
+import logging
+
+week_day = {'понедьдик': 0,
+            'вторник': 1,
+            'среда': 2,
+            'четверг': 3,
+            'пятница': 4,
+            'суббота': 5,
+            'воскресенье': 6}
+
+dict_monat = {'января': 1,
+              'февраля': 2,
+              'марта': 3,
+              'апреля': 4,
+              'мая': 5,
+              'июня': 6,
+              'июля': 7,
+              'августа': 8,
+              'сентября': 9,
+              'октября': 10,
+              'ноября': 11,
+              'декабря': 12}
+
+FORMAT = '{asctime} {levelname} {funcName}->{lineno}: {msg}'
+
+logging.basicConfig(filename='log/log.txt', format=FORMAT, style='{', level=logging.INFO, encoding='utf-8')
+logger = logging.getLogger(__name__)
+
 
 def get_date(date_text):
-    text_ = f'{datetime.now().year}'
-    text_date = datetime.strptime(date_text, f'{text_} %A. Время %H:%M:%S. Это %W неделя и %j день года.')
+    try:
+        d, w, m, = date_text.split(' ')
+        if d >= 6:
+            logger.error('не верный ввод')
+    except Exception:
+        logger.error('не верный ввод')
 
-
-
+    d = int(d.strip("-й"))
+    try:
+        weekday = week_day[w]
+        month_ = dict_monat[m]
+    except KeyError:
+        logger.error('не верный ввод')
+    count = 0
+    for day in range(1, 32):
+        try:
+            data_res = datetime(datetime.now().year, month_, day)
+        except UnboundLocalError:
+            logger.error('не верный ввод')
+        try:
+            if data_res.weekday() == weekday and count != d:
+                count += 1
+            if data_res.weekday() == weekday and count == d:
+                return data_res
+        except Exception as e:
+            logger.error(f'не номер дня недели{e}')
+            return None
 
 if __name__ == '__main__':
-    print(get_date('1-й четверг ноября')
-
-
-
-
-d = datetime.now()
-date_text = 'Дата 15 June 2007. День недели Friday. Время 02:14:00. Это 24 неделя и 166 день года.'
-text_date = datetime.strptime(date_text, 'Дата %d %B %Y. День недели %A. Время %H:%M:%S. Это %W неделя и %j день года.')
-print(text_date)
-print(d.year)
-print(d.weekday())
-print(d.day)
+    print(get_date('5-й четверг марта'))
--- Seminar_15/Task_5.py
+++ Seminar_15/Task_5.py
@@ -8,3 +8,65 @@
 названия дня недели и месяца, но и числовые,
 т.е не мая, а 5.
 """
+
+from datetime import datetime
+import logging
+
+week_day = {'понедьдик': 0,
+            'вторник': 1,
+            'среда': 2,
+            'четверг': 3,
+            'пятница': 4,
+            'суббота': 5,
+            'воскресенье': 6}
+
+dict_monat = {'января': 1,
+              'февраля': 2,
+              'марта': 3,
+              'апреля': 4,
+              'мая': 5,
+              'июня': 6,
+              'июля': 7,
+              'августа': 8,
+              'сентября': 9,
+              'октября': 10,
+              'ноября': 11,
+              'декабря': 12}
+
+FORMAT = '{asctime} {levelname} {funcName}->{lineno}: {msg}'
+
+logging.basicConfig(filename='log/log.txt', format=FORMAT, style='{', level=logging.INFO, encoding='utf-8')
+logger = logging.getLogger(__name__)
+
+
+def get_date(date_text):
+    try:
+        d, w, m, = date_text.split(' ')
+        if d >= 6:
+            logger.error('не верный ввод')
+    except Exception:
+        logger.error('не верный ввод')
+
+    d = int(d.strip("-й"))
+    try:
+        weekday = week_day[w]
+        month_ = dict_monat[m]
+    except KeyError:
+        logger.error('не верный ввод')
+    count = 0
+    for day in range(1, 32):
+        try:
+            data_res = datetime(datetime.now().year, month_, day)
+        except UnboundLocalError:
+            logger.error('не верный ввод')
+        try:
+            if data_res.weekday() == weekday and count != d:
+                count += 1
+            if data_res.weekday() == weekday and count == d:
+                return data_res
+        except Exception as e:
+            logger.error(f'не номер дня недели{e}')
+            return None
+
+if __name__ == '__main__':
+    print(get_date('5-й четверг марта'))
