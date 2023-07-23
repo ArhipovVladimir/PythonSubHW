0f64eec47e2a77ac210208acbba356089c663b2b
ArhipovVS <arhipof05@yndex.ru>
2023-07-23 11:26:19 +0700

    Seminar 15 HW 2

--- Seminar_15/Task_1.py
+++ Seminar_15/Task_1.py
@@ -3,4 +3,24 @@
 вывода сообщения об ошибке в файл.
 Например отлавливаем ошибку деления на ноль."""
 
+import logging
 
+FORMAT = '{levelname:<8} - {asctime}. В модуле "{name}" ' \
+            'в строке {lineno:03d} функция "{funcName}()" ' \
+            'в {created} секунд записала сообщение: {msg}'
+
+# FORMAT = '{asctime} {levelname} {funcName}->{lineno}: {msg}'
+
+logging.basicConfig(filename='log/log.txt', format=FORMAT, style='{', level=logging.DEBUG, encoding='utf-8')
+logger = logging.getLogger('main')
+logger.debug("ошибка")
+
+def div_(a, b):
+    return a / b
+
+
+if __name__ == '__main__':
+    a, b = map(int, input("введите число").split(' '))
+    print(a)
+    print(b)
+    print(div_(a, b))
--- Seminar_15/Task_2.py
+++ Seminar_15/Task_2.py
@@ -4,3 +4,51 @@
 работы в файл.
 Напишите аналогичный декоратор, но внутри используйте
 модуль logging."""
+
+"""Задание №3
+Напишите декоратор, который сохраняет в json файл
+параметры декорируемой функции и результат, который она
+возвращает. При повторном вызове файл должен
+расширяться, а не перезаписываться.
+Каждый ключевой параметр сохраните как отдельный ключ
+json словаря.
+Для декорирования напишите функцию, которая может
+принимать как позиционные, так и ключевые аргументы.
+Имя файла должно совпадать с именем декорируемой
+функции."""
+import json
+import os
+from typing import Callable
+from functools import wraps
+import logging
+
+# FORMAT = '{levelname:<8} - {asctime}. В модуле "{name}" ' \
+#             'в строке {lineno:03d} функция "{funcName}()" ' \
+#             'в {created} секунд записала сообщение: {msg}'
+
+FORMAT = '{asctime} {levelname} {funcName}->{lineno}: {msg}'
+
+logging.basicConfig(filename='log/log.txt', format=FORMAT, style='{', level=logging.DEBUG, encoding='utf-8')
+logger = logging.getLogger('main')
+logger.debug("ошибка")
+
+def save_log(func):
+    @wraps(func)
+    def wrapper(num, *arg, **kwargs):
+
+        result = func(num, *arg, **kwargs)
+        logging.basicConfig(filename='log/log.txt', format=FORMAT, style='{', level=logging.DEBUG, encoding='utf-8')
+        logger = logging.getLogger('main')
+        logger.debug({'args': (num, arg, kwargs), 'result': result})
+
+
+    return wrapper
+
+
+@save_log
+def get_sum(num, *arg, **kwargs):
+    return num
+
+if __name__ == '__main__':
+    get_sum(1, 2, 3)
+
